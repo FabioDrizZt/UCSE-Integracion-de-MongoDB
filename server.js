8b328a7e84a03b1d624c14511dbfe6ff5cfcea5a
fabio <fabio>
2025-06-02 17:40:16 -0300

    rutas de frutas

--- server.js
+++ server.js
@@ -7,12 +7,23 @@ app.get('/', (req, res) => {
   res.json({message: "Hoal mundo"})
 })
 
-app.get('/movies', async (req, res) => {
+app.get('/frutas', async (req, res) => {
   const client = await connectToDB()
-  const db = client.db('movies')
-  const movies = await db.collection('movies').find().toArray()
+  const db = client.db('frutas')
+  const frutas = await db.collection('frutas').find().toArray()
   await disconnectFromDB()
-  res.json ({movies})
+  res.json ({frutas})
+})
+
+app.get('/frutas/:id', async (req, res) => {
+    const frutasId = parseInt(req.params.id) || 0
+  const client = await connectToDB()
+  const db = client.db('frutas')
+  const fruta = await db.collection('frutas').findOne({id: frutasId})
+  await disconnectFromDB();
+    (fruta) ? res.json(fruta) 
+    : res.status(404).json({error:'404 - fruta not found'})
+  
 })
 
 app.listen(port, () => {

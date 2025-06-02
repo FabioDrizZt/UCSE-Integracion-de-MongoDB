9586cc6639501518d242980044677d0fe955c3f6
fabio <fabio>
2025-06-02 17:54:35 -0300

    middleware de conexión y try catch

--- server.js
+++ server.js
@@ -3,27 +3,47 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
+app.use(async (req,res,next) => {
+    try {
+        const client = await connectToDB()
+        req.db = client.db('frutas')
+        next()
+    } catch {
+        console.log('Error al conectarse a la DB');
+    }
+})
+
 app.get('/', (req, res) => {
   res.json({message: "Hoal mundo"})
 })
 
 app.get('/frutas', async (req, res) => {
-  const client = await connectToDB()
-  const db = client.db('frutas')
-  const frutas = await db.collection('frutas').find().toArray()
-  await disconnectFromDB()
-  res.json ({frutas})
+    try {
+        const frutas = await req.db.collection('frutas').find().toArray()
+        res.json ({frutas})
+    } catch {
+        console.log('error al traer la collection de frutas');
+    } finally {
+        await disconnectFromDB()
+    }
 })
 
 app.get('/frutas/:id', async (req, res) => {
-    const frutasId = parseInt(req.params.id) || 0
-  const client = await connectToDB()
-  const db = client.db('frutas')
-  const fruta = await db.collection('frutas').findOne({id: frutasId})
-  await disconnectFromDB();
-    (fruta) ? res.json(fruta) 
-    : res.status(404).json({error:'404 - fruta not found'})
-  
+    try {
+        const frutasId = parseInt(req.params.id) || 0
+        const fruta = await req.db.collection('frutas').findOne({id: frutasId})
+        (fruta) ? res.json(fruta) 
+        : res.status(404).json({error:'404 - fruta not found'})
+    } catch {
+        console.log('error al traer la collection de frutas');
+    } finally {
+        await disconnectFromDB();
+    }
+    
+})
+
+app.use((req,res) => {
+    res.status(404).json({message: 'Endpoint not found'})
 })
 
 app.listen(port, () => {
